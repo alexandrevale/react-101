3177e3ceef2304efe5e38a4a4636e6bb46373f8e
Alexandre do Vale <vale.apv@gmail.com>
2019-01-28 23:13:29 -0200

    Configuração do package.json e página de teste

---
+++ react_webpack/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Componenet } from 'react';
+
+class App extends Componenet {
+	render() {
+		return (
+			<div>
+				<h1>React App</h1>
+			</div>
+		)
+	}
+}
+
+export default App;

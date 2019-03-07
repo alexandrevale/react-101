5d5e98a665dec99fa9405ae13c9d8e120c10b0e4
Alexandre do Vale <vale.apv@gmail.com>
2019-03-07 19:31:27 -0300

    Component Home (sem conteúdo definitivo)

---
+++ portfolio/components/home/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Row, Col, Card } from 'react-materialize';
+
+const Home = () => (
+  <Row>
+    <Col m={4} s={12}></Col>
+    <Col m={8} s={12}>
+        <h5 className="subtitle">Objetivo</h5>
+        <Card>
+          <div>
+
+          </div>
+        </Card>
+        <h5 className="subtitle">Experiência</h5>
+    </Col>
+  </Row>
+);
+
+export default Home;
